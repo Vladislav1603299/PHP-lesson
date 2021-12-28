1c3f511479d8db32f9709e67d58262a71cbceb00
lesson1 <abinin99@bk.ru>
2021-12-29 01:39:55 +0300

    add homework6

--- task3/fullfotos.php
+++ task3/fullfotos.php
@@ -12,30 +12,31 @@
         <h1 class="header_name">Ваше фото <?= $_GET['img'] ?></h1>
     </header>
     <main class="main_foto">
-    <?php 
-    include_once "config.php";
+    <?php
+    include_once 'config.php';
     $id = $_GET['id'];
     $getsql = "SELECT count FROM fotos WHERE id=$id";
     $getCount = mysqli_query($connect, $getsql);
     $sqlUpdate = "UPDATE fotos SET count= count + 1 WHERE id=$id";
     mysqli_query($connect, $sqlUpdate);
-    
 
-    if($data = mysqli_fetch_assoc($getCount)){?>
+    if ($data = mysqli_fetch_assoc($getCount)) { ?>
         <p>Это фото смотрели <?= $data['count'] ?></p><br>    
-<?php
-    }
- ?>   
+<?php }
+    ?>   
     <a style="color: red" class="end" href="<?= $_SERVER[
-            'HTTP_REFERER'
-        ] ?>">Вернуться назад</a><br><br>
+        'HTTP_REFERER'
+    ] ?>">Вернуться назад</a><br><br>
 
 
-<a href="server.php?id=<?= $_GET['id']?>&action=delete"><button>Удалить фото</button></a><br><br>
+<a href="server.php?id=<?= $_GET[
+    'id'
+] ?>&action=delete"><button>Удалить фото</button></a><br><br>
 <h3>Нажми на картинку, чтобы увеличить</h3>
         <img class="foto" src="img/<?= $_GET[
             'img'
         ] ?>" alt="foto" style="width:500px;" onclick="this.style.width='1250px'" onmouseout="this.style.width='500px'" ><br><br>
+        
     </main>
     <footer>
 
--- task3/gallery.php
+++ task3/gallery.php
@@ -4,12 +4,13 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style/style.css">
+    <link rel="stylesheet" href="style/styles.css">
     <title>Gallery</title>
 </head>
 <body>
     <header class="header">
-        <h1 class="header_name">My Gallery</h1>
+        <h1 class="header_name">Моя галерея</h1>
+        <a style="text-decoration: none; margin-right: 15px; color: white" href="feedback.php">Отзывы</a>
     </header>
     <main class="main">
         <form class="main_block_loading" action="server.php" method="post" enctype="multipart/form-data">
@@ -20,14 +21,16 @@
             </div>
         </form>
     <?php
-        include_once "config.php";
-        $sql = "SELECT * FROM fotos ORDER BY count desc";
-        $res = mysqli_query($connect, $sql);
-        while($data = mysqli_fetch_assoc($res)){?>
-            <a href="fullfotos.php?id=<?= $data['id'];?>&img=<?= $data['name_foto'];?>"><img width="200px" height="100px" src="img/<?= $data['name_foto'];?>" alt="foto"></a>
-    <?php 
-        
-    }
+    include_once 'config.php';
+    $sql = 'SELECT * FROM fotos ORDER BY count desc';
+    $res = mysqli_query($connect, $sql);
+    while ($data = mysqli_fetch_assoc($res)) { ?>
+            <a href="fullfotos.php?id=<?= $data['id'] ?>&img=<?= $data[
+    'name_foto'
+] ?>"><img width="200px" height="100px" src="img/<?= $data[
+    'name_foto'
+] ?>" alt="foto"></a>
+    <?php }
     ?>     
     </main> 
     <footer class="footer">
